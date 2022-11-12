9816ba86916b328ff52b80fcffe9ba89068ad5dc
Yinghai Yu <Y15653862659@outlook.com>
2022-11-12 14:52:27 -0800

    week3day2-homework

---
+++ week3day1-homework/data.js
@@ -0,0 +1,293 @@
+const countries1 = [
+    {
+        'country': 'Afghanistan', 'continent': 'Asia'
+    },
+    {
+        'country': 'Argentina', 'continent': 'South America'
+    },
+    {
+        'country': 'Australia', 'continent': 'Australia'
+    },
+    {
+        'country': 'Austria', 'continent': 'Europe'
+    },
+    {
+        'country': 'Bahamas', 'continent': 'North America'
+    },
+    {
+        'country': 'Bangladesh', 'continent': 'Asia'
+    },
+    {
+        'country': 'Belgium', 'continent': 'Europe'
+    },
+    {
+        'country': 'Bolivia', 'continent': 'South America'
+    },
+    {
+        'country': 'Brazil', 'continent': 'South America'
+    },
+    {
+        'country': 'Bulgaria', 'continent': 'Europe'
+    }
+]
+    
+const countries2 = [
+    {
+        'country': 'Cambodia', 'continent': 'Asia'
+    },
+    {
+        'country': 'Canada', 'continent': 'North America'
+    },
+    {
+        'country': 'Chile', 'continent': 'South America'
+    },
+    {
+        'country': 'China', 'continent': 'Asia'
+    },
+    {
+        'country': 'Columbia', 'continent': 'South America'
+    },
+    { 
+        'country': 'Costa Rica', 'continent': 'North America'
+    },
+    {
+        'country': 'Denmark', 'continent': 'Europe'
+    },
+    {
+        'country': 'Dominican Republic', 'continent': 'North America'
+    },
+    {
+        'country': 'Ecuador', 'continent': 'South America'
+    }
+]
+    
+const countries3 = [
+    {
+        'country': 'Egypt', 'continent': 'Africa'
+    },
+    {
+        'country': 'Ethopia', 'continent': 'Africa'
+    },
+    {
+        'country': 'Finland', 'continent': 'Europe'
+    },
+    {
+        'country': 'France', 'continent': 'Europe'
+    },
+    {
+        'country': 'Germany', 'continent': 'Europe'
+    },
+    {
+        'country': 'Ghana', 'continent': 'Africa'
+    },
+    {
+        'country': 'Greece', 'continent': 'Europe'
+    },
+    {
+        'country': 'Grenada', 'continent': 'North America'
+    },
+    {
+        'country': 'Guatemala', 'continent': 'North America'
+    },
+    {
+        'country': 'Haiti', 'continent': 'North America'
+    }
+]
+
+const countries4 = [
+    {
+        'country': 'Honduras', 'continent': 'North America'
+    },
+    {
+        'country': 'Hungary', 'continent': 'Europe'
+    },
+    {
+        'country': 'India', 'continent': 'Asia'
+    },
+    {
+        'country': 'Indonesia', 'continent': 'Asia'
+    },
+    {
+        'country': 'Iran', 'continent': 'Asia'
+    },
+    {
+        'country': 'Iraq', 'continent': 'Asia'
+    },
+    {
+        'country': 'Ireland', 'continent': 'Europe'
+    },
+    {
+        'country': 'Israel', 'continent': 'Europe'
+    }
+]
+    
+const countries5 = [
+    {
+        'country': 'Italy', 'continent': 'Europe'
+    },
+    {
+        'country': 'Jamaica', 'continent': 'North America'
+    },
+    {
+        'country': 'Japan', 'continent': 'Asia'
+    },
+    {
+        'country': 'Kenya', 'continent': 'Africa'
+    },
+    {
+        'country': 'Kuwait', 'continent': 'Asia'
+    },
+    {
+        'country': 'Laos', 'continent': 'Asia'
+    },
+    {
+        'country': 'Lebanon', 'continent': 'Asia'
+    },
+    {
+        'country': 'Malawi', 'continent': 'Africa'
+    },
+    {
+        'country': 'Malaysia', 'continent': 'Asia'
+    },
+    {
+        'country': 'Mexico', 'continent': 'North America'
+    }
+]
+
+const countries6 = [
+    {
+        'country': 'Morocco', 'continent': 'Africa'
+    },
+    {
+        'country': 'Nepal', 'continent': 'Asia'
+    },
+    {
+        'country': 'Netherlands', 'continent': 'Europe'
+    },
+    {
+        'country': 'New Zealand', 'continent': 'Australia'
+    },
+    {
+        'country': 'Nigeria', 'continent': 'Africa'
+    },
+    {
+        'country': 'Norway', 'continent': 'Europe'
+    },
+    {
+        'country': 'Pakistan', 'continent': 'Asia'
+    },
+    {
+        'country': 'Panama', 'continent': 'North America'
+    },
+    {
+        'country': 'Paraguay', 'continent': 'South America'
+    }
+]
+
+const countries7 = [
+    {
+        'country': 'Peru', 'continent': 'South America'
+    },
+    {
+        'country': 'Phillipines', 'continent': 'Asia'
+    },
+    {
+        'country': 'Poland', 'continent': 'Europe'
+    },
+    {
+        'country': 'Portugal', 'continent': 'Europe'
+    },
+    {
+        'country': 'Romania', 'continent': 'Europe'
+    },
+    {
+        'country': 'Russia', 'continent': 'Europe'
+    },
+    {
+        'country': 'Saint Lucia', 'continent': 'North America'
+    },
+    {
+        'country': 'Saudi Arabia', 'continent': 'Asia'
+    },
+    {
+        'country': 'Sierra Leone', 'continent': 'Africa'
+    },
+    {
+        'country': 'Singapore', 'continent': 'Asia'
+    },
+    {
+        'country': 'Slovakia', 'continent': 'Europe'
+    },
+    {
+        'country': 'Slovenia', 'continent': 'Europe'
+    },
+    {
+        'country': 'Somalia', 'continent': 'Africa'
+    }
+]
+
+const countries8 = [
+    {
+        'country': 'South Africa', 'continent': 'Africa'
+    },
+    {
+        'country': 'South Korea', 'continent': 'Asia'
+    },
+    {
+        'country': 'Spain', 'continent': 'Europe'
+    },
+    {
+        'country': 'Sri Lanka', 'continent': 'Asia'
+    },
+    {
+        'country': 'Sweden', 'continent': 'Europe'
+    },
+    {
+        'country': 'Switzerland', 'continent': 'Europe'
+    },
+    {
+        'country': 'Syria', 'continent': 'Europe'
+    },
+    {
+        'country': 'Tanzania', 'continent': 'Africa'
+    },
+    {
+        'country': 'Thailand', 'continent': 'Asia'
+    },
+    {
+        'country': 'Turkey', 'continent': 'Europe'
+    },
+    {
+        'country': 'Uganda', 'continent': 'Africa'
+    }
+]
+
+const countries9 = [
+    {
+        'country': 'Ukraine', 'continent': 'Europe'
+    },
+    {
+        'country': 'United Arab Amirates', 'continent': 'Asia'
+    },
+    {
+        'country': 'United Kingdom', 'continent': 'Europe'
+    },
+    {
+        'country': 'United States of America', 'continent': 'North America'
+    },
+    {
+        'country': 'Uruguay', 'continent': 'South America'
+    },
+    {
+        'country': 'Venezuela', 'continent': 'South America'
+    },
+    {
+        'country': 'Vietnam', 'continent': 'Asia'
+    },
+    {
+        'country': 'Yemen', 'continent': 'Africa'
+    },
+    {
+        'country': 'Zimbabwe', 'continent': 'Africa'
+    }
+]
