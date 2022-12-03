18975711ccd50625cb493b0cebe94b75f83f7f68
Yinghai Yu <Y15653862659@outlook.com>
2022-12-03 14:10:30 -0800

    week6day1-homework

---
+++ week6day1-homework/index.js
@@ -0,0 +1,111 @@
+// EXPRESS SETUP
+const express = require('express');
+const app = require();
+
+app.use(express.static(__dirname));
+
+const bodyParse = require('body-parser');
+const expressSession = require('express-session')({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+});
+
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(expressSession);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('App listening on port ' + port));
+
+
+// PASSPORT SETUP
+const passport = require('passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// MONGOOSE SETUP
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+mongoose.connect('mongodb://localhost/MyDatabase',
+    {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+    username: String,
+    password: String,
+});
+
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+
+// PASSPORT LOCAL AUTHENTICATION
+passport.use(UserDetails.createStrategy());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+
+// ROUTES
+const connectEnsureLogin = require('connect-ensure-login');
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.redirect('/login?info=' + info);
+        }
+        req.logIn(user, function(err) {
+            if (err) {
+                return next(err);
+            }
+            return res.redirect('/');
+        });
+    }) (req, res, next);
+});
+
+app.get('/login', (req, res) => res.sendFile('html/login.html', { root: __dirname }));
+
+app.get('/', connectEnsureLogin.ensureLoggedIn('html/index.html', {root: __dirname}));
+
+app.get('/private', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    res.sendFile('html/private.html', { root: __dirname })
+});
+
+app.get('/user', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    res.send({ user: req.user })
+});
+
+app.get('/logout', (req, res) => {
+    req.logout(),
+    res.sendFile('html/logout.html',
+    {root: __dirname})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
