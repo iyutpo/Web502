d4758deb4f38a6173543c0dc795e2dc6c8b63552
Yinghai Yu <Y15653862659@outlook.com>
2022-12-15 20:41:00 -0800

    week8day2-demo

---
+++ week8day2-demo/js/sw.js
@@ -0,0 +1,41 @@
+
+var cacheName = 'hello-pwa';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/styles.css',
+    '/js/main.js',
+];
+
+
+// start the service worker and cache all of the app's content
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    )
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    )
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
